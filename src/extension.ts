2ce37190a5116669fc053316ccfbe59922cb4081
yuelu <942893986@qq.com>
2025-05-10 18:40:53 +0800

    feat: 初始化 yuelu-translate-by-cursor 插件

---
+++ src/extension.ts
@@ -0,0 +1,53 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+import * as vscode from 'vscode';
+
+// This method is called when your extension is activated
+// Your extension is activated the very first time the command is executed
+export function activate(context: vscode.ExtensionContext) {
+  // Use the console to output diagnostic information (console.log) and errors (console.error)
+  // This line of code will only be executed once when your extension is activated
+  console.log(
+    'Congratulations, your extension "yuelu-translate" is now active!',
+  );
+
+  // The command has been defined in the package.json file
+  // Now provide the implementation of the command with registerCommand
+  // The commandId parameter must match the command field in package.json
+  const disposable = vscode.commands.registerCommand(
+    'yuelu-translate.helloWorld',
+    () => {
+      // The code you place here will be executed every time your command is executed
+      // Display a message box to the user
+      vscode.window.showInformationMessage('Hello World from 月鹿翻译!');
+    },
+  );
+
+  // 注册翻译命令
+  const translateCommand = vscode.commands.registerCommand(
+    'yuelu-translate.translateToEnglish',
+    () => {
+      // 获取活动编辑器
+      const editor = vscode.window.activeTextEditor;
+
+      if (editor) {
+        // 获取选中的文本
+        const selection = editor.selection;
+        const text = editor.document.getText(selection);
+
+        if (text.length > 0) {
+          // 暂时返回"功能开发中"
+          vscode.window.showInformationMessage('功能开发中');
+        } else {
+          vscode.window.showWarningMessage('请先选择要翻译的文本');
+        }
+      }
+    },
+  );
+
+  context.subscriptions.push(disposable);
+  context.subscriptions.push(translateCommand);
+}
+
+// This method is called when your extension is deactivated
+export function deactivate() {}
